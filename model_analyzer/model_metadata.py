4e584a69cc6e81b1008b5dfceb0c316c905540b7
Artyom Tugaryov <artyom.tugarev@intel.com>
2022-01-13 07:44:17 +0300

    Some fixes

--- model_analyzer/model_metadata.py
+++ model_analyzer/model_metadata.py
@@ -15,7 +15,7 @@
 import logging
 from contextlib import suppress
 from pathlib import Path
-from typing import Union, Dict, Optional, Tuple, List
+from typing import Dict, Optional, Tuple, List
 from xml.etree import ElementTree
 
 # pylint: disable=import-error
@@ -38,15 +38,14 @@ class ModelMetaData:
         # Load network to get execution graph if needed before Constant Folding (WA for PriorBox)
         self.int8precisions, self.int8layers = self.get_exec_graph_int8layers()
 
-        self.constant_folding()
+        self._constant_folding()
 
         self.is_onnx = model_path.suffix in ('.onnx', '.prototxt')
         self.xml = None if self.is_onnx else ElementTree.parse(model_path)
 
-        self.output_layers = [output.get_node() for output in self.model.outputs]
         self.input_layers = [model_input.node for model_input in self.model.inputs]
 
-    def constant_folding(self):
+    def _constant_folding(self):
         try:
             pass_manager = Manager()
             pass_manager.register_pass('ConstantFolding')
@@ -79,15 +78,13 @@ class ModelMetaData:
     def get_framework(self):
         return self.xml.find('./meta_data/cli_parameters/framework').attrib['value']
 
-    def get_ie_outputs(self) -> List[str]:
+    @property
+    def outputs(self) -> List[Node]:
         return [model_outputs.node for model_outputs in self.model.outputs]
 
-    def get_ie_inputs(self) -> list:
+    @property
+    def input_names(self) -> List[str]:
         return [model_input.node.name for model_input in self.model.inputs]
-
-    @staticmethod
-    def _get_output_shape(layer: Node, port: int = 0) -> List[int]:
-        return get_shape_for_node_safely(layer.output(port))
 
     def get_layer_types(self) -> list:
         return [layer.get_type_name() for layer in self.ops]
@@ -95,7 +92,7 @@ class ModelMetaData:
     def find_input_info_layer(self) -> str:
         """Return the name of the IMAGE_INFO layer. Instance segmentation only."""
         result = None
-        for layer in self.get_ie_inputs():
+        for layer in self.input_names:
             if self.model.input_info[layer].layout == LayoutTypes.NC.value:
                 result = layer
                 break
@@ -104,9 +101,8 @@ class ModelMetaData:
     def analyze_inpainting_inputs(self) -> Dict[str, str]:
         """Return input predictions for image and mask layers. Inpainting only."""
         roles = {}
-        inputs = self.get_ie_inputs()
-        for candidate in inputs:
-            layer = self.network.input_info[candidate]
+        for index, candidate in enumerate(self.input_names):
+            layer = self.model.input(index)
             shape = self.get_shape_values(layer.layout, layer.input_data.shape)
             if layer.layout == LayoutTypes.NCHW.value and shape['C'] == 1:
                 roles['mask'] = candidate
@@ -118,8 +114,8 @@ class ModelMetaData:
     def analyze_super_resolution_inputs(self) -> Dict[str, str]:
         """Return input predictions for low-res and upsampling inputs. Super-resolution only."""
         roles = {}
-        if len(self.input_layers) == 1:
-            roles['low-res'] = self.input_layers[0].get_friendly_name()
+        if len(self.input_names) == 1:
+            roles['low-res'] = self.input_names[0]
             return roles
 
         dims = {}
@@ -133,13 +129,12 @@ class ModelMetaData:
 
         return roles
 
-    def analyze_output_roles(self) -> Union[Dict[str, str], None]:
+    def analyze_output_roles(self) -> Optional[Dict[str, str]]:
         """Return role predictions for output layers. Instance Segmentation only."""
         roles = {}
-        outputs = self.get_ie_outputs()
         framework = self.get_framework()
         if framework == 'onnx':
-            for candidate in outputs:
+            for candidate in self.outputs:
                 precision = candidate.get_output_element_type(0).get_type_name()
                 if precision in {'i32', 'i16'}:
                     roles['classes_out'] = candidate
@@ -153,7 +148,7 @@ class ModelMetaData:
                 if self.network.outputs[candidate].layout == LayoutTypes.NCHW.value:
                     roles['raw_masks_out'] = candidate
         if framework == 'tf':
-            for candidate in outputs:
+            for candidate in self.outputs:
                 if self.network.outputs[candidate].layout == LayoutTypes.NC.value:
                     roles['detection_out'] = candidate
                     continue
@@ -166,7 +161,7 @@ class ModelMetaData:
         """Extract model params from the output layer of the model. YOLOv2/TinyYOLOv2 only."""
         params = {}
         relevant_attributes = ['classes', 'coords', 'num']
-        output_attributes = self.output_layers[0].get_attributes()
+        output_attributes = self.outputs[0].get_attributes()
         for attribute in relevant_attributes:
             params[attribute] = output_attributes.get(attribute)
 
@@ -174,7 +169,7 @@ class ModelMetaData:
 
     def is_argmax_used(self):
         """Return info on whether the network output is argmaxed. Semantic Segmentation only"""
-        output_layer = self.get_ie_outputs()[0]
+        output_layer = self.outputs[0]
         output_shape = self.get_shape_values(output_layer.layout, output_layer.shape)
 
         return output_shape['C'] == 1
@@ -220,7 +215,7 @@ class ModelMetaData:
 
     def get_num_classes(self) -> Optional[int]:
         """Return number of classes the IR supports, if possible."""
-        if len(self.output_layers) != 1:
+        if len(self.outputs) != 1:
             return None
 
         layer_types = self.get_layer_types()
@@ -244,10 +239,10 @@ class ModelMetaData:
 
     def has_background_class(self) -> Optional[bool]:
         """Return True if the IR supports background class, None if unknown."""
-        if len(self.output_layers) != 1:
+        if len(self.outputs) != 1:
             return None
 
-        output = self.output_layers[0]
+        output = self.outputs[0]
 
         if isinstance(output, Node):
             output_type = output.get_type_name().lower()
@@ -270,7 +265,7 @@ class ModelMetaData:
         return True if indicator else None
 
     @staticmethod
-    def get_shape_values(layouts: list, shapes: list) -> dict:
+    def get_shape_values(layouts: list, shapes: list) -> Dict[str, int]:
         shape_values = {
             'N': None,
             'C': None,
@@ -283,13 +278,13 @@ class ModelMetaData:
         return shape_values
 
     def _get_anchors(self) -> Optional[List[float]]:
-        region_yolo = [layer for layer in self.output_layers if layer.get_type_name() == 'RegionYolo']
+        region_yolo = [layer for layer in self.outputs if layer.get_type_name() == 'RegionYolo']
         if region_yolo:
             return region_yolo[0].get_attributes().get('anchors', [])
         return None
 
     def yolo_has_raw_output(self) -> bool:
-        return 'RegionYolo' not in [layer.get_type_name() for layer in self.output_layers if layer.get_type_name()]
+        return 'RegionYolo' not in [layer.get_type_name() for layer in self.outputs if layer.get_type_name()]
 
     def _is_yolo(self) -> bool:
         layer_types = set(self.get_layer_types())
@@ -321,18 +316,18 @@ class ModelMetaData:
         Criteria: single image input and outputs with proportional shapes
         (or single image output with odd number of cells).
         """
-        if self._all_outputs_are_image() and len(self.output_layers) == output_count:
+        if self._all_outputs_are_image() and len(self.outputs) == output_count:
             return self._check_single_image_input() and self._check_output_shape_proportions()
 
         return False
 
     def _check_single_image_input(self) -> bool:
-        if not len(self.input_layers) == 1:
+        if not len(self.input_names) == 1:
             return False
-        return len(get_shape_for_node_safely(self.input_layers[0].output(0))) == 4
+        return len(get_shape_for_node_safely(self.model.input)) == 4
 
     def _all_outputs_are_image(self) -> bool:
-        return all(len(self._get_output_shape(output)) == 4 for output in self.output_layers)
+        return all(len(self._get_output_shape(output)) == 4 for output in self.outputs)
 
     def _check_output_shape_proportions(self) -> bool:
         """
@@ -340,7 +335,7 @@ class ModelMetaData:
         Example: [1, 255, 17, 17], [1, 255, 34, 34]
         For single output, check if it has an odd number of cells in a row/col (as it is common practice).
         """
-        output_shapes = [self._get_output_shape(output) for output in self.output_layers]
+        output_shapes = [self._get_output_shape(output) for output in self.outputs]
 
         if len(output_shapes) == 1:
             return ModelMetaData._is_yolo_shape(output_shapes[0])
@@ -387,7 +382,7 @@ class ModelMetaData:
         return ModelTypes.GENERIC
 
     def _is_classification(self) -> bool:
-        if len(self.output_layers) != 1:
+        if len(self.outputs) != 1:
             return False
 
         layer_types = set(self.get_layer_types())
@@ -397,7 +392,7 @@ class ModelMetaData:
         if not valid_layer_types:
             return False
 
-        out_layer = self.output_layers[0]
+        out_layer = self.outputs[0]
         out_shape = self._get_output_shape(out_layer)
 
         if len(out_shape) == 2:
@@ -409,7 +404,7 @@ class ModelMetaData:
 
     def _is_ssd(self) -> bool:
         layer_types = set(self.get_layer_types())
-        output_types = {layer.get_type_name() for layer in self.output_layers}
+        output_types = {layer.get_type_name() for layer in self.outputs}
 
         return 'ROIPooling' not in layer_types and 'DetectionOutput' in output_types
 
@@ -418,7 +413,7 @@ class ModelMetaData:
             return False
 
         layer_types = set(self.get_layer_types())
-        output_types = {layer.get_type_name() for layer in self.output_layers}
+        output_types = {layer.get_type_name() for layer in self.outputs}
 
         xml_layers = list(self.xml.getroot().find('layers'))
         xml_layer_types = [layer.attrib.get('type') for layer in xml_layers]
@@ -428,8 +423,12 @@ class ModelMetaData:
         return ('ROIPooling' in layer_types and 'DetectionOutput' not in output_types) \
                or 'ExperimentalDetectronROIFeatureExtractor' in xml_layer_types
 
+    def _get_layout_for_input(self, input_index: int) -> Optional[str]:
+        layout = self.model.input(input_index).node.layout
+        return layout
+
     def _is_semantic_segmentation(self) -> bool:
-        if len(self.output_layers) != 1:
+        if len(self.outputs) != 1:
             return False
 
         convolutions = [layer for layer in self.ops if layer.get_type_name() in ['Convolution', 'GroupConvolution']]
@@ -438,22 +437,24 @@ class ModelMetaData:
 
         layers_types = self.get_layer_types()
 
-        input_layer = next(iter(self.model.inputs))
+        input_layer = self.model.input()
 
-        input_shape = self.get_shape_values(input_layer.layout, input_layer.input_data.shape)
-
-        output_layer = self.output_layers[0]
+        output_layer = self.outputs[0]
         output_shape = self._get_output_shape(output_layer)
         output_dim = list(output_shape)[-2:]
 
+        input_layout = self._get_layout_for_input(0) # has only one input
+
+        input_shape = self.get_shape_values(input_layout, input_layer.shape)
         input_dim = [input_shape['H'], input_shape['W']]
+
         equal_dims = bool(input_dim == output_dim)
 
         return equal_dims and len(dilations) > 1 and 'Elu' not in layers_types
 
     def _is_inpainting(self) -> bool:
         layers_types = set(self.get_layer_types())
-        inputs = self.get_ie_inputs()
+        inputs = self.input_names
 
         return 'Elu' in layers_types and len(inputs) == 2
 
@@ -463,12 +464,12 @@ class ModelMetaData:
         return 'MVN' in layers_types
 
     def _is_super_resolution(self) -> bool:
-        single_stream = len(self.input_layers) == 1 and len(self.output_layers) == 1
-        double_stream = len(self.input_layers) == 2 and len(self.output_layers) == 1
+        single_stream = len(self.input_names) == 1 and len(self.outputs) == 1
+        double_stream = len(self.input_names) == 2 and len(self.outputs) == 1
 
         input_shapes = [get_shape_for_node_safely(candidate) for candidate in self.input_layers]
         output_shape = [self._get_output_shape(candidate)
-                        for candidate in self.output_layers][0]
+                        for candidate in self.outputs][0]
 
         # Super-resolution network should return a valid RGB/grayscale image
         # Check the number of color channels and output dimensions
@@ -494,10 +495,10 @@ class ModelMetaData:
                 1) Uses PRelu activation functions or separate L2 regularization;
                 2) Single output with NC shape.
         """
-        if len(self.output_layers) != 1:
+        if len(self.outputs) != 1:
             return False
 
-        output_layer = self.output_layers[0]
+        output_layer = self.outputs[0]
         output_shapes = self._get_output_shape(output_layer)
         layers_types = set(self.get_layer_types())
 
@@ -513,8 +514,8 @@ class ModelMetaData:
         layers_types = set(self.get_layer_types())
 
         reduced_dims = False
-        if len(self.output_layers) == 1:
-            output_layer = self.output_layers[0]
+        if len(self.outputs) == 1:
+            output_layer = self.outputs[0]
             output_shapes = self._get_output_shape(output_layer)
             reduced_dims = output_shapes[2] == output_shapes[3] == 1
 
@@ -564,3 +565,7 @@ class ModelMetaData:
 
     def is_model_dynamic(self) -> bool:
         return self.model.is_dynamic()
+
+    @staticmethod
+    def _get_output_shape(layer: Node, port: int = 0) -> List[int]:
+        return get_shape_for_node_safely(layer.output(port))
